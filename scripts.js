8387e7f828b1c3690a81d6f33a307698d05073ca
cmac2992 <cmac2992@gmail.com>
2014-09-20 13:50:54 -0700

    kelly map ux, choose page

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
 function initialize() {
             var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
                 var mapOptions = {
                   center: myLatlng,
-                  zoom: 8
+                  zoom: 8,
+                    disableDefaultUI: true
                 };
 
             var map = new google.maps.Map(document.getElementById("map-canvas"),
@@ -11,12 +16,11 @@ function initialize() {
             marker.setMap(null);
             // var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/'
 
-            // var marker = new google.maps.Marker({
-            //       position: myLatlng,
-            //       map: map,
-            //       title: 'Hello World!'
-            //       // icon: iconBase;
-            // });
+            var marker = new google.maps.Marker({
+                  position: myLatlng,
+                  map: map,
+                  title: 'Hello World!'
+            });
 
             google.maps.event.addListener(map, 'center_changed', function() {
                 // 0.1 seconds after the center of the map has changed,
@@ -24,7 +28,7 @@ function initialize() {
                 window.setTimeout(function() {
                   var center = map.getCenter();
                   marker.setPosition(center);
-                }, 100);
+                }, 0);
             });
         }
       google.maps.event.addDomListener(window, 'load', initialize);
