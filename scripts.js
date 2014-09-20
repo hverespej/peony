986ce8e605abd9d7a0082d63f1afa3dbc5a252d6
cmac2992 <cmac2992@gmail.com>
2014-09-20 10:47:54 -0700

    add bootstrap, add stylsheet, add map page

---
+++ scripts.js
@@ -0,0 +1,26 @@
+function initialize() {
+            var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+                var mapOptions = {
+                  center: myLatlng,
+                  zoom: 8
+                };
+
+            var map = new google.maps.Map(document.getElementById("map-canvas"),
+                mapOptions);
+
+            var marker = new google.maps.Marker({
+                  position: myLatlng,
+                  map: map,
+                  title: 'Hello World!'
+            });
+
+            google.maps.event.addListener(map, 'center_changed', function() {
+                // 0.1 seconds after the center of the map has changed,
+                // set back the marker position.
+                window.setTimeout(function() {
+                  var center = map.getCenter();
+                  marker.setPosition(center);
+                }, 100);
+            });
+        }
+      google.maps.event.addDomListener(window, 'load', initialize);
